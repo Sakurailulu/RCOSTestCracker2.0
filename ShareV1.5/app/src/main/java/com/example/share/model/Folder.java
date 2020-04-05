2d924ae06ec59cd341eb16bb0231763e2b8160c3
Sakurailulu <34151383+Sakurailulu@users.noreply.github.com>
2020-04-05 17:41:29 -0400

    Create Folder.java

---
+++ ShareV1.5/app/src/main/java/com/example/share/model/Folder.java
@@ -0,0 +1,108 @@
+package com.example.share.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Folder implements Serializable {
+
+    private Integer id;
+    private Course course;
+    private User user;
+
+    private String title;
+    private String content;
+    private String path;
+    private String fileName;
+    private Date createTime;
+    private Integer likeNum;
+    private Integer dislikeNum;
+    private Integer state;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Integer getLikeNum() {
+        return likeNum;
+    }
+
+    public void setLikeNum(Integer likeNum) {
+        this.likeNum = likeNum;
+    }
+
+    public Integer getDislikeNum() {
+        return dislikeNum;
+    }
+
+    public void setDislikeNum(Integer dislikeNum) {
+        this.dislikeNum = dislikeNum;
+    }
+
+    public Integer getState() {
+        return state;
+    }
+
+    public void setState(Integer state) {
+        this.state = state;
+    }
+}
