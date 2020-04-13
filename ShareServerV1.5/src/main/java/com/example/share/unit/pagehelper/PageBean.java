1155666d1a765269c6bcd3a43170dae93054663e
Sakurailulu <34151383+Sakurailulu@users.noreply.github.com>
2020-04-12 23:30:48 -0400

    Create PageBean.java

---
+++ ShareServerV1.5/src/main/java/com/example/share/unit/pagehelper/PageBean.java
@@ -0,0 +1,55 @@
+package com.example.share.unit.pagehelper;
+
+import com.github.pagehelper.Page;
+
+import java.util.List;
+
+public class PageBean {
+
+    public PageBean(Page page) {
+        this.pageNum = page.getPageNum();
+        this.pageSize = page.getPageSize();
+        this.total = page.getTotal();
+        this.data = page.getResult();
+    }
+
+    private int pageNum;    //current page
+
+    private int pageSize;   //page size
+
+    private long total;     //data length
+
+    private List data;      //data
+
+    public int getPageNum() {
+        return pageNum;
+    }
+
+    public void setPageNum(int pageNum) {
+        this.pageNum = pageNum;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public long getTotal() {
+        return total;
+    }
+
+    public void setTotal(long total) {
+        this.total = total;
+    }
+
+    public List getData() {
+        return data;
+    }
+
+    public void setData(List data) {
+        this.data = data;
+    }
+}
